06bffdb3b200d417e11908cff3c08fb511d65ea5
yoyoo <mcwatney@naver.com>
2024-04-27 18:19:07 +0900

    zsh, brew 추가

---
+++ brew/package.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+# zsh
+brew install zsh
+
+# Iterm2
+brew install --cask iterm2
+
+# Chrome
+brew install --cask google-chrome
+
+# Slack
+brew install --cask slack
+
+# Discord
+brew install --cask discord
+
+
+# Postman
+brew install --cask postman
+
+# Docker
+brew install --cask docker
+
+# Notion
+brew install --cask notion
+
+
+# zsh-completions
+brew install zsh-completions
+
+# zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
+
+
+# Mise
+# curl https://mise.run | sh
+
+# Kakaotalk
+# brew install mas
+# mas search kakaotalk
+# mas signin ..
